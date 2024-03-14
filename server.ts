5b7dc69ca6259e55ea838379fb4c33038bdb0d7d
omoinjm <njmcloud@gmail.com>
2024-03-14 23:12:24 +0200

    let's see x4

--- server.ts
+++ server.ts
@@ -1,9 +1,69 @@
-const express = require("express");
+//const express = require("express");
+//
+//const app = express();
+//
+//app.get("/", (req, res) => res.send("Express on Vercel"));
+//
+//app.listen(3000, () => console.log("Server ready on port 3000."));
+//
+//module.exports = app;
 
-const app = express();
+// server.ts
+import express, { Router, Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+/*
+ * Load up and parse configuration details from
+ * the `.env` file to the `process.env`
+ * object of Node.js
+ */
+dotenv.config();
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+/*
+ * Create an Express application and get the
+ * value of the PORT environment variable
+ * from the `process.env`
+ */
+const api: Express = express();
+const router = Router();
+const port = process.env.PORT || 3000;
 
-module.exports = app;
+// Static Files
+api.use(express.static(__dirname + "/assets"));
+api.use("/css", express.static(__dirname + "assets/css"));
+
+// Set Templating Engine
+api.set("views", __dirname + "/views");
+api.set("view engine", "ejs");
+
+router.get("/", (req: Request, res: Response) => {
+  res.render("pages/home", {
+    title: "Email Template Api",
+    isHome: true,
+  });
+});
+
+router.get("/template", (req: Request, res: Response) => {
+  // Extract the template name fom query parameters
+  const { template_name, first_name } = req.query;
+
+  const requiredParams = ["template_name", "first_name", "last_name"];
+  const missingParams = requiredParams.filter((param) => !req.query[param]);
+
+  if (missingParams.length > 0) {
+    return res.status(400).json({ error: "Missing parameters", missingParams });
+  }
+
+  res.render(`pages/${template_name}`, {
+    title: "Thank you",
+    isHome: false,
+    displayName: first_name,
+  });
+});
+
+api.use("/api/", router);
+
+/* Start the Express app and listen for incoming requests on the specified port */
+api.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
